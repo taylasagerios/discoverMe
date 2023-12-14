949af625be4e7b27e0f083b6e20ba7cabe842ed7
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-12-13 22:46:12 -0800

    pulled in helpfull files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from './NavTabs';
+export default function Header() {
+  return (
+    <div>
+    <h1 className='me row'>Alonso Ampuero</h1>
+    <div className='row mx-0 align-items-center'>
+      <Nav />
+    </div>
+    
+    </div>
+  );
+}
---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+  <>
+  <div className="row text-center justify-content-center mx-0">
+    <h2 className="text">About Me</h2>
+    <img className="profile" src="Profile.jpg"/>
+  </div>
+  <div className="row">
+    <div className="text text-center">
+      <br />
+      <p id="about-me" className="form-control">
+        I was born in Lima, Peru and we moved to the States in 1998 where we lived in GA for most of my life.<br />
+        I went to School at Mercer University in GA for Computer Engineering and Computer Science, but didn't quite finish.<br />
+        After school, I worked a variety of food service jobs and eventually became a manager of a coffee shop in Macon, GA.<br />
+        I then moved to Portland, OR for 4 years where I worked at a retail store as a clerk and then assistant manager.<br />
+        Due to the deteriating health of my grandmother worsening I moved back to GA in May 2023.<br />
+        This gave me a kick in the pants to revitilize my desire to work as a developer and join a coding bootcamp.
+        </p>
+    </div>
+    <div>
+
+    </div>
+  </div>
+  </>
+  )
+}
