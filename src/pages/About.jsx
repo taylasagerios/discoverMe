a936250c4993de7c3c27aa7b78e2d457ee37df46
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-12-13 23:14:53 -0800

    edited old file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,24 @@
 export default function About() {
   return (
-  <>
-  <div className="row text-center justify-content-center mx-0">
-    <h2 className="text">About Me</h2>
-    <img className="profile" src="/assets/87D2FF0C-197D-4C3D-8074-998FBCE153E0.jpg"/>
-  </div>
-  <div className="row">
-    <div className="text text-center">
-      <br />
-      <p id="about-me" className="form-control">
-Hello my name is Tayla Sage Rios I am new full stack web developer. <br/>
-I also have a degree in Political science from Foothill College.<br/>
- I am born and raised in Northern California. I am the youngest in a family of 5.<br/> 
- Coding, law and music are my passions. I hope you enjoy my site and feel free to connect and reach out. 
-        </p>
-    </div>
-    <div>
+    <>
+      <div className="row text-center justify-content-center mx-0">
+        <h2 className="text">About Me</h2>
+        <img className="profile" src="/assets/87D2FF0C-197D-4C3D-8074-998FBCE153E0.jpg" />
+      </div>
+      <div className="row">
+        <div className="text text-center">
+          <br />
+          <p id="about-me" className="form-control">
+            Hello my name is Tayla Sage Rios I am new full stack web developer. <br />
+            I also have a degree in Political science from Foothill College.<br />
+            I am born and raised in Northern California. I am the youngest in a family of 5.<br />
+            Coding, law and music are my passions. I hope you enjoy my site and feel free to connect and reach out.
+          </p>
+        </div>
+        <div>
 
-    </div>
-  </div>
-  </>
+        </div>
+      </div>
+    </>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,15 +9,11 @@ export default function Projects(){
     </div>
     
     <div className="row mt-4">
+        <Project title={"Scenery Finder"} image="project1.jpg" repo="https://github.com/eviehoang/scenery-finder" deploy="https://eviehoang.github.io/scenery-finder/"/>    
         <Project title={"MySequel Adventure"} image="project2.jpg" repo="https://github.com/taylasagerios/my-sequel-adventure" deploy="https://my-sequel-adventure-a9247b63bc9a.herokuapp.com/"/>
-        <Project title={"Food Finder"} image="project1.jpg" repo="https://github.com/FenriRagni/food-finder" deploy="https://fenriragni.github.io/food-finder/"/>
-        <Project title={"Just Another Text Editor"} image="jate.jpg" repo="https://github.com/FenriRagni/text-editor" deploy="https://super-duper-text-editor.onrender.com/"/>
+        <Project title={"Project 3 coming soon"} />
     </div>
-    <div className="row mt-4">
-      <Project title={'Note Taker'} image='notes.jpg' repo="https://github.com/FenriRagni/make-a-note" deploy="https://make-a-note-907915b8410c.herokuapp.com/"/>
-      <Project title={'Weather'} image='weather.jpg' repo="https://github.com/FenriRagni/and-now-the-weather" deploy="https://fenriragni.github.io/and-now-the-weather/"/>
-      <Project title={'Password Generator'} image='pwd.jpg' repo="https://github.com/FenriRagni/pwd-gen" deploy="https://fenriragni.github.io/pwd-gen/"/>
-    </div>
+  
     </>
   )
 }
